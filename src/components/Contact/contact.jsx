db09a8be2fded15845053399988edd5cb997b2c0
Rafaela <rafaelaffigueiredo1@gmail.com>
2024-03-19 20:40:23 -0300

    consertando direcionamento do email

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -18,18 +18,18 @@ export const Contact = () => {
             </div>
             <ul className={styles.links}>
                 <li className={styles.link}>
-                    <a href="rafaelaffigueiredo1@gmail.com"  target="_blank">
-                    <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+                    <a href="mailto:rafaelaffigueiredo1@gmail.com"  target="_blank">
+                        <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
                     </a>
                 </li>
                 <li className={styles.link}>
                     <a href="https://www.linkedin.com/in/rafaelaffigueiredo/"  target="_blank">
-                    <img src={getImageUrl("contact/linkedinIcon.png")} alt="Linkedin icon" />
+                        <img src={getImageUrl("contact/linkedinIcon.png")} alt="Linkedin icon" />
                     </a>
                 </li>
                 <li className={styles.link}>
                     <a href="https://github.com/figueiredorafaela"  target="_blank">
-                    <img src={getImageUrl("contact/githubIcon.png")} alt="github icon" />
+                        <img src={getImageUrl("contact/githubIcon.png")} alt="github icon" />
                     </a>
                 </li>
             </ul>
